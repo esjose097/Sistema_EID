dd74f1af7525a3d8b82fe49ad43d6886c7af42d8
Eliu <supergonzalez360@gmail.com>
2021-04-09 23:32:15 -0700

    Logo

--- Pantallas/src/Frames/MateriaPrima_Entradas_Salidas.java
+++ Pantallas/src/Frames/MateriaPrima_Entradas_Salidas.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
